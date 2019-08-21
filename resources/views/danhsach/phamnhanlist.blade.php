3f6217e9e55c9b9e3bb767425ef927ad9e7d319e
TRANCONGVU <vndvip10>
2019-08-21 19:57:08 +0700

    ASS_LARAVEL

--- resources/views/danhsach/phamnhanlist.blade.php
+++ resources/views/danhsach/phamnhanlist.blade.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-    <title>Document</title>
-</head>
-
-<body>
+@extends('layout')
+@section('body')
     <div class="container-flui bg-dark header">
         <div class="container">
 
@@ -63,6 +51,4 @@
 
         </div>
     </div>
-</body>
-
-</html>
+@endsection
