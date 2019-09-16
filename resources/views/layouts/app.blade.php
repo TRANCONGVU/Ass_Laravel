3b4d5681ec4a746c2420879d842d73d7fa5e6a53
TRANCONGVU <vndvip10>
2019-09-16 19:12:45 +0700

    trang chu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,6 @@
                     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
                         <div class="container">
                             <a class="navbar-brand" href="{{ url('/') }}">
-
                                 Quản lý nhà tù
                             </a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
